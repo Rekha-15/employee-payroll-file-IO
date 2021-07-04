42a471c969d64b41320c16916057129caab30769
Rekha-15 <rekhapatil.1509@gmail.com>
2021-07-04 19:57:07 -0400

    added : Creating a Watch Service to watch particular directory along with all Files and Sub Directories : .

--- src/main/java/com/Employee_Payroll_IO/FileOperations.java
+++ src/main/java/com/Employee_Payroll_IO/FileOperations.java
@@ -7,19 +7,17 @@
 package com.Employee_Payroll_IO;
 
 import java.io.IOException;
-
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.IntStream;
 
 /**
- * FileOperations is a class of public type.
- * IN this class operating different functions, like deleting , existent checking ext
+ * @author rekha
  *
  */
 public class FileOperations {
-    private static final String HOME = System.getProperty("Path", "C:\\Users\\PLAY_WITH_NIO");
+    private static final String HOME = System.getProperty("Path", "C:\\Users\\rekha\\Developer\\Employee_Payroll_IO");
     private static final String PLAY_WITH_NIO = "TempPlayGround";
 
     public void fileOperationDemonstrator() throws IOException {
@@ -39,7 +37,7 @@ public class FileOperations {
 
         //Create File
         IntStream.range(1, 10).forEach(counter -> {
-            Path tempFile = Paths.get(playPath + "\\temp" + counter);
+            Path tempFile = Paths.get(playPath + "\\temp" + counter+".txt");
             System.out.println("is tempFile exists?1" + Files.notExists(tempFile));
             try {
                 Files.createFile(tempFile);
