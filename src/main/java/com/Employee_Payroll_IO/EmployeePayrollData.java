ceac31db5c85e6de659b3bbd62929d7fef261ae2
Rekha-15 <rekhapatil.1509@gmail.com>
2021-07-04 03:32:46 -0400

    added : Creating a Employee Payroll Service to Read and Write Employee Payroll to a Console : .

--- src/main/java/com/Employee_Payroll_IO/EmployeePayrollData.java
+++ src/main/java/com/Employee_Payroll_IO/EmployeePayrollData.java
@@ -1,5 +1,40 @@
+/***************************************************************
+ *@purpose Implementing Employee Payroll Service Problem .
+ *@author Rekha
+ *@version 1.0
+ *@since 3-07-2021
+ **************************************************************/
+
 package com.Employee_Payroll_IO;
 
-public class EmployeePayrollData {
+/**
+ * EmployeePayrollData is a class of public type Contains employee Id,employee
+ * name and employee salary
+ * 
+ * @author rekha
+ *
+ */
+public class EmployeePayRollData {
+	/**
+	 * Instance variable id, name salary
+	 */
+    int id;
+    String name;
+    Double salary;
+    
+    /**Constructor for EmployeePayrollData.
+     *@param id;
+     *@param name;
+     * @param salary
+      */
 
+    public EmployeePayRollData(int id, String name, Double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+    
+    public String toString() {
+    	return " name=+'" +name + '\'' + "id=" + id +  ",salary=" + salary;
+    	}
 }
--- src/main/java/com/Employee_Payroll_IO/EmployeePayrollService.java
+++ src/main/java/com/Employee_Payroll_IO/EmployeePayrollService.java
@@ -1,9 +1,67 @@
+/***************************************************************
+ *@purpose Implementing Employee Payroll Service Problem .
+ *@author Rekha
+ *@version 1.0
+ *@since 3-07-2021
+ **************************************************************/
 package com.Employee_Payroll_IO;
 
-public class EmployeePayrollService {
-	
-	    public static void main( String[] args )
-	    {
-	        System.out.println( "Employee Payroll Service Problem" );
-	    }
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class EmployeePayRollService {
+    public enum IOService {
+        CONSOLE_IO, FILE_IO, DB_IO, REST_IO
+    }
+    private List<EmployeePayRollData> employeePayRollList;
+
+    /**
+     * Default Constructor
+     */
+    public EmployeePayRollService() {
+    }
+
+    /**
+     * Parameterized Constructor for Initializing List
+     */
+    public EmployeePayRollService(List<EmployeePayRollData> employeePayRollList) {
+        this.employeePayRollList = employeePayRollList;
+    }
+
+    /**
+     * to Read from the Console
+     * @param consoleInputReader Scanner Object
+     */
+    private void readEmployeePayRollData(Scanner consoleInputReader) {
+        System.out.println("Please Enter Employee Name");
+        String name = consoleInputReader.nextLine();
+        System.out.println("Please Enter Employee ID");
+        int id = consoleInputReader.nextInt();
+        System.out.println("Please Enter Employee Salary");
+        double salary = consoleInputReader.nextDouble();
+
+        employeePayRollList.add(new EmployeePayRollData(id, name, salary));
+    }
+
+    /**
+     * to Write into the Console
+     */
+    private void writeEmployeePayRollData() {
+        System.out.println("\n Writing Employee Payroll Roster to Console\n" + employeePayRollList);
+    }
+
+    /**
+     * main method
+     * prints welcome message
+     * in a EmployeePayRollService class created employeePayRollService object
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Payroll service program!");
+        ArrayList<EmployeePayRollData> employeePayRollList = new ArrayList<>();
+        EmployeePayRollService employeePayRollService = new EmployeePayRollService(employeePayRollList);
+        Scanner consoleInputReader = new Scanner(System.in);
+        employeePayRollService.readEmployeePayRollData(consoleInputReader); // scanning input from console
+        employeePayRollService.writeEmployeePayRollData();                  // writing back to console
+    }
 }
